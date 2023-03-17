67d632ad5fd35f03bf122f411b0ba88e68440853
Joe Crosby <64789836+joeyc2224@users.noreply.github.com>
2023-03-17 17:11:02 +0000

    Added a data plan two types of posts

---
+++ data-plan.js
@@ -0,0 +1,43 @@
+userProfile = {
+    userID: 46778,
+    username: "joey",
+    dateJoined: "26/3/22",
+    closeFriends: {
+        userIDs: [
+            12342,
+            67644,
+            12577,
+            86756,
+        ],
+        usernames: [
+            "claire",
+            "dan",
+            "bobby",
+            "jimbo"
+        ]
+    }
+}
+
+checkInPost = {
+    userID: 46778,
+    username: "joeyc123",
+    jorunalID: 45674,
+    share: true,
+    time: Date.now(),
+    moodRating: "Happy",
+    journalLog: "A good day......",
+    image: "7653.jpg",
+    journalStreak: 12
+}
+
+feedPost = {
+    userID: 46778,
+    username: "joeyc123",
+    postID: 739122,
+    time: Date.now(),
+    category: "meditation",
+    image: "",
+    video: "5340.mp4",
+    caption: "A 10 minute meditation session I....",
+    likes: 55,
+}
