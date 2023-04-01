24c3660ca20b1ba1328e1e499779a6aea4112e97
Joe Crosby <64789836+joeyc2224@users.noreply.github.com>
2023-04-01 20:46:05 +0100

    Basic login working with MongoDB
    Login pages addded and working with session timer. need to add styling

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+const mongoDBPassword = process.env.MYMONGODBPASSWORD
+const sessionSecret = process.env.MYSESSIONSECRET
+
+
 //express server setup - do node.js in term to run
 const express = require('express')
 const app = express()
 app.listen(3000, () => console.log('listening on port 3000'))
 
 //server html pages from public folder
-app.use(express.static('public'))
+app.use(express.static('public', {index: 'user-views/login.html'}))
 
 app.use(express.json())
 
@@ -12,17 +17,131 @@ app.use(express.urlencoded({ extended: false }));
 
 const path = require('path');
 
-const postData = require('./posts-data.js')
 
+//consts to hold expiry times in ms
+const tensecs = 1000 * 10;
+const threeMins = 1000 * 60 * 3;
+const oneHour = 1000 * 60 * 60;
+
+//use the sessions module and the cookie parser module
+const sessions = require('express-session');
+const cookieParser = require("cookie-parser");
+
+//make cookie parser middleware available
+app.use(cookieParser());
+
+//load sessions middleware, with some config
+app.use(sessions({
+    secret: sessionSecret,
+    saveUninitialized: true,
+    cookie: { maxAge: tensecs },
+    resave: false
+}));
+
+
+//add mongoose(MongoDB) module to connect to DB
+const mongoose = require('mongoose');
+//password from .env file
+mongoose.connect("mongodb+srv://joeyc123:" + mongoDBPassword + "@karunadb.onlhvad.mongodb.net/Karuna?retryWrites=true&w=majority")
+
+//data models import
+const users = require('./models/users')
+const postData = require('./models/posts.js')
+
+//user check login function
+function checkLoggedIn(request, response, nextAction) {
+    if (request.session) {
+        if (request.session.userid) {
+            nextAction()
+        } else {
+            request.session.destroy()
+            return response.redirect('/user-views/no-login.html')
+        }
+    }
+}
+
+//controller for the main app view, depends on user logged in state
+app.get('/app', checkLoggedIn, (request, response) => {
+    // response.redirect('./application.html')
+    response.redirect('home.html')
+})
+
+app.get('/feed', checkLoggedIn, (request, response) => {
+    // response.redirect('./application.html')
+    response.redirect('feed.html')
+})
+
+app.get('/post', checkLoggedIn, (request, response) => {
+    // response.redirect('./application.html')
+    response.redirect('post.html')
+})
+
+app.get('/logout', async (request, response) => {
+    await users.setLoggedIn(request.session.userid, false)
+    request.session.destroy()
+    await console.log(users.getUsers())
+    response.redirect('/user-views/login.html')
+})
+
+app.post('/pagecheck', async (request, response) => {
+    await users.setLoggedIn(request.session.userid, false)
+    request.session.destroy()
+    await console.log(users.getUsers())
+    response.redirect('/user-views/no-login.html')
+})
+
+//controller for login
+app.post('/login', async (request, response) => {
+    let userData = request.body
+    console.log(userData)
+
+    if (await users.findUser(userData.username)) {
+        console.log('user found')
+        if (await users.checkPassword(userData.username, userData.password)) {
+            console.log('password matches')
+            request.session.userid = userData.username
+            await users.setLoggedIn(userData.username, true)
+            response.redirect('home.html')
+        } else {
+            console.log('password wrong')
+            response.redirect('/user-views/failed-login.html')
+        }
+    } else {
+        console.log('no such user')
+        response.redirect('/user-views/failed-login.html')
+    }
+})
+
+//controller for registering a new user
+app.post('/register', async (request, response) => {
+    console.log(request.body)
+    let userData = request.body
+    // console.log(userData.username)
+    if (await users.findUser(userData.username)) {
+        console.log('user exists')
+        response.json({
+            status: 'failed',
+            error: 'user exists'
+        })
+    } else {
+        users.newUser(userData.username, userData.password)
+        response.redirect('/home.html')
+    }
+    console.log(users.getUsers())
+})
+
+
+//posting/posts functions
 app.post('/newpost', (request, response) => {
     console.log(request.body)
     postData.addNewPost(request.body)
     response.redirect('/feed.html')
 })
 
-app.get('/getposts', (request, response) => {
-    //console.log(postData.getPosts(3))
+app.get('/getposts', async (request, response) => {
     response.json({
-        posts: postData.getPosts()
+        posts: await postData.getPosts()
     })
 })
+
+
---
+++ models/posts.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema({
+    postedBy: String,
+    video: String,
+    caption: String,
+    likes: Number,
+    time: Date,
+    day: Number,
+    comments: [{
+        commentBy: String,
+        comment: String,
+        time: Date,
+    }]
+})
+
+const Posts = model('Posts', postSchema);
+
+function addNewPost(post) {
+    let myPost = {
+        postedBy: "Username",
+        video: post.videoSelect + "#autoplay;loop;hide-title;",
+        caption: post.message,
+        likes: 5,
+        time: Date.now(),
+        day: 2,
+        comments: [{
+            commentBy: "Jimmy",
+            comment: "Nice vid mate, really good",
+        }]
+    }
+    //create new collection data in mongo
+    Posts.create(myPost)
+        .catch(err => {
+            console.log("Error: " + err)
+        })
+}
+
+//return posts
+async function getPosts(n = 20) {
+    let data = []
+    await Posts.find({})
+        .sort({ 'time': -1 })
+        .limit(n)
+        .exec()
+        .then(mongoData => {
+            data = mongoData;
+        })
+        .catch(err => {
+            console.log('Error:' + err)
+        });
+    return data;
+}
+
+module.exports = { addNewPost, getPosts }
